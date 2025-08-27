6dec73857e4ab0f6b0ed1043420324e30df1f9e4
Ninjarashad815 <rashad.bryant@jpschools.org>
2025-08-27 17:40:08 +0000

    add portfoli page to website

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,7 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "Navy"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,10 +27,27 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
+createPlatform(150,700 ,65 ,12 , "black");
+createPlatform(250,620 ,40 ,12 , "black");
+createPlatform(400,500,65 ,7 , "black");
+createPlatform(295,400 ,65 ,7 , "black");
+createPlatform(400,295 ,300,8 , "color");
+createPlatform(403,200 ,448 ,8 , "white");
+createPlatform(700,350 ,45 ,7 , "black");
+createPlatform(750,300 ,45 ,7 , "color");
+createPlatform(465,300 ,8 ,450 , "black");
+createPlatform(795,300 ,8 ,80 , "black");
+createPlatform(850,200 ,4 ,375 , "black");
+createPlatform(550,575 ,550 ,7 , "black");
+createPlatform(469,475 ,332 ,6 , "black");
+createPlatform(500 , 750 ,332 ,6 , "black");
+
+
+
 
 
 
@@ -41,7 +58,9 @@ $(function () {
 
     
     // TODO 4 - Create Cannons
-
+createCannon("top", 200, 1000);
+createCannon("right", 262, 3500);
+createCannon("right", 658, 3600);
 
     
     
